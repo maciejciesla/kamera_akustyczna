95d3adcb44471f398432d0f3d59da9659e83588a
maciejciesla <macciesla5@gmail.com>
2017-11-05 22:45:15 +0100

    pliki1
    1 czesc

---
+++ kamera_akustyczna_v0.1.cache/ip/2017.3/b43929d327486932/kolejka_util_ds_buf_0_0_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2017.3 (win64) Build 2018833 Wed Oct  4 19:58:22 MDT 2017
+-- Date        : Fri Nov  3 13:44:36 2017
+-- Host        : DESKTOP-QHJIUEA running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+--               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ kolejka_util_ds_buf_0_0_stub.vhdl
+-- Design      : kolejka_util_ds_buf_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z010clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+  Port ( 
+    IBUF_DS_P : in STD_LOGIC_VECTOR ( 0 to 0 );
+    IBUF_DS_N : in STD_LOGIC_VECTOR ( 0 to 0 );
+    IBUF_OUT : out STD_LOGIC_VECTOR ( 0 to 0 )
+  );
+
+end decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix;
+
+architecture stub of decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "IBUF_DS_P[0:0],IBUF_DS_N[0:0],IBUF_OUT[0:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "util_ds_buf,Vivado 2017.3";
+begin
+end;
